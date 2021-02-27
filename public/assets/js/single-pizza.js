21f08688e6f0c7cde75e267540e25cd4e7764bc0
Nick Hill <nickhill2380@gmail.com>
2021-02-27 16:36:41 -0500

    added ability to add pizzas while offline

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -10,24 +10,27 @@ const $newCommentForm = document.querySelector('#new-comment-form');
 let pizzaId;
 
 function getPizza() {
-  // get id of Pizza
+  // get id of pizza
   const searchParams = new URLSearchParams(document.location.search.substring(1));
   const pizzaId = searchParams.get('id');
 
   // get pizzaInfo
   fetch(`/api/pizzas/${pizzaId}`)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error({ message: 'Something went wrong!'});
-    }
-    return response.json();
-  })
-  .then(printPizza)
-  .catch(err => {
-    console.log(err);
-    alert('Cannot find a pizza with this id! Taking you back.');
-    window.history.back();
-  });
+    .then(response => {
+      console.log(response);
+      if (!response.ok) {
+        console.log('hi');
+        throw new Error({ message: 'Something went wrong!' });
+      }
+
+      return response.json();
+    })
+    .then(printPizza)
+    .catch(err => {
+      console.log(err);
+      alert('Cannot find a pizza with this id! Taking you back.');
+      window.history.back();
+    });
 }
 
 function printPizza(pizzaData) {
@@ -109,8 +112,8 @@ function handleNewCommentSubmit(event) {
 
   const formData = { commentBody, writtenBy };
 
-  fetch(`/api/comments/${pizzaId}`,{
-  method: 'POST',
+  fetch(`/api/comments/${pizzaId}`, {
+    method: 'POST',
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json'
@@ -118,14 +121,14 @@ function handleNewCommentSubmit(event) {
     body: JSON.stringify(formData)
   })
     .then(response => {
-      if(!response.ok) {
+      if (!response.ok) {
         throw new Error('Something went wrong!');
       }
       response.json();
     })
     .then(commentResponse => {
       console.log(commentResponse);
-      location.reload();
+      // location.reload();
     })
     .catch(err => {
       console.log(err);
@@ -158,19 +161,19 @@ function handleNewReplySubmit(event) {
     },
     body: JSON.stringify(formData)
   })
-  .then(response => {
-    if(!response.ok) {
-      throw new Error('Something went wrong!');
-    }
-    response.json();
-  })
-  .then(commentResponse => {
-    console.log(commentResponse);
-    location.reload();
-  })
-  .catch(err => {
-    console.log(err);
-  });
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+      response.json();
+    })
+    .then(commentResponse => {
+      console.log(commentResponse);
+      location.reload();
+    })
+    .catch(err => {
+      console.log(err);
+    });
 }
 
 $backBtn.addEventListener('click', function() {
