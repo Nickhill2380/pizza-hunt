91cd2d55be6f557cd5a0b0cb41af3651aad2abb3
Nick Hill <nickhill2380@gmail.com>
2021-02-27 10:57:56 -0500

    added comments feature

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -42,6 +42,7 @@ const handlePizzaSubmit = event => {
   const pizzaName = $pizzaForm.querySelector('#pizza-name').value;
   const createdBy = $pizzaForm.querySelector('#created-by').value;
   const size = $pizzaForm.querySelector('#pizza-size').value;
+  // transform DOM data into a real array of objects to execute .map()
   const toppings = [...$pizzaForm.querySelectorAll('[name=topping]:checked')].map(topping => {
     return topping.value;
   });
@@ -51,7 +52,8 @@ const handlePizzaSubmit = event => {
   }
 
   const formData = { pizzaName, createdBy, size, toppings };
-  fetch('api/pizzas', {
+
+  fetch('/api/pizzas', {
     method: 'POST',
     headers: {
       Accept: 'application/json',
@@ -59,14 +61,14 @@ const handlePizzaSubmit = event => {
     },
     body: JSON.stringify(formData)
   })
-  .then(response=> response.json())
-  .then(postResponse => {
-    alert('Pizza created successfully!');
-    console.log(postResponse);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+    .then(response => response.json())
+    .then(postResponse => {
+      alert('Pizza created successfully!');
+      console.log(postResponse);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 $pizzaForm.addEventListener('submit', handlePizzaSubmit);
